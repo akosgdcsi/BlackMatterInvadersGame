e8b4fffa897ccad3cd18133598829fadfb6b5de0
Gadacsi Akos <akosgadacsi99@gmail.com>
2021-04-15 15:16:34 +0200

    gamobjects created

---
+++ ShipSumo/BlackMatter.Model/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackMatter.Model
+{
+    public class Bullet : GameObject
+    {
+        public bool IsCollided { get; set; }
+    }
+}
---
+++ ShipSumo/BlackMatter.Model/GameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackMatter.Model
+{
+    public abstract class GameObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+}
