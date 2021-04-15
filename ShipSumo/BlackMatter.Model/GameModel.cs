3457c0df371afb2b94c3d5c753b3e8199f891ea5
Gadacsi Akos <akosgadacsi99@gmail.com>
2021-04-16 01:01:44 +0200

    player enemy bullet movement and shooting added

--- ShipSumo/BlackMatter.Model/GameModel.cs
+++ ShipSumo/BlackMatter.Model/GameModel.cs
@@ -10,17 +10,23 @@ namespace BlackMatter.Model
     {
         public Player player { get; set; }
         public List<Enemy> enemies { get; set; }
+        public List<Bullet> PlayerBullets { get; set; }
+        public List<Bullet> EnemyBullets { get; set; }
         public double GameWidth { get; set; }
         public double GameHeight { get; set; }
         public int Wave { get; set; }
+        public int Enemiesinthiswave { get; set; }
 
-        public GameModel(Player player, List<Enemy> enemies, double gameWidth, double gameHeight, int Wave)
+        public GameModel(Player player, List<Enemy> enemies, List<Bullet> playerBullets, List<Bullet> enemyBullets, double gameWidth, double gameHeight, int Wave, int Enemiesinthiswave)
         {
             this.player = player;
             this.enemies = enemies;
+            this.PlayerBullets = playerBullets;
+            this.EnemyBullets = enemyBullets;
             GameWidth = gameWidth;
             GameHeight = gameHeight;
             this.Wave = Wave;
+            this.Enemiesinthiswave = Enemiesinthiswave;
         }
     }
 }
