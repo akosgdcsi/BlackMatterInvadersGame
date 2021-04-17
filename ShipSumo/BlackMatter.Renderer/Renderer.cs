64ab0a2553bddb2d9cae22d376fe2538d1000e29
Lencsés Dávid <lencses.david77@gmail.com>
2021-04-17 20:14:16 +0200

    Model and renderer working in progress

---
+++ ShipSumo/BlackMatter.Renderer/Renderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using BlackMatter.Model;
+
+
+namespace BlackMatter.Renderer
+{
+    public class Renderer
+    {
+        GameModel model;
+        Dictionary<string, Brush> brushes = new Dictionary<string, Brush>();
+        Drawing BackGround;
+
+        public Renderer(GameModel model)
+        {
+            this.model = model;
+            
+        }
+        Brush GetBrush(string fname)
+        {
+            if (!brushes.ContainsKey(fname))
+            {
+                BitmapImage bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = Assembly.GetExecutingAssembly().GetManifestResourceStream(fname);
+                bmp.EndInit();
+                ImageBrush ib = new ImageBrush(bmp);
+                brushes.Add(fname, ib);
+            }
+            return brushes[fname];
+        }
+
+        Brush PlayerBrush { get { return GetBrush("BlackMatter.Renderer.Images.player_ship.png"); } }
+
+        public Drawing BuildDrawing()
+        {
+            DrawingGroup dg = new DrawingGroup();
+            dg.Children.Add(GetBackground());
+        }
+
+        private Drawing GetBackground()
+        {
+            if(BackGround ==null)
+            {
+                Geometry g = new RectangleGeometry(new Rect(0, 0, model.GameWidth, model.GameHeight));
+                BackGround = new GeometryDrawing(Brushes.Black, null, g);
+            }
+            return BackGround;
+        }
+    }
+}
