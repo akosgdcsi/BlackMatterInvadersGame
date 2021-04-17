d97e184ee77ce32ff096788410aa3fdc992e58ef
Lencsés Dávid <lencses.david77@gmail.com>
2021-04-18 01:42:51 +0200

    Renderer nearly done

--- ShipSumo/BlackMatter.Renderer/Renderer.cs
+++ ShipSumo/BlackMatter.Renderer/Renderer.cs
@@ -15,6 +15,15 @@ namespace BlackMatter.Renderer
         GameModel model;
         Dictionary<string, Brush> brushes = new Dictionary<string, Brush>();
         Drawing BackGround;
+        Drawing Player;
+        Drawing Enemy;
+        Drawing Bullet;
+        Drawing Text;
+        Pen red = new Pen(Brushes.Red, 2);
+        Typeface font = new Typeface("Arial");
+        Point textLocation = new Point(0, 1);
+        FormattedText formattedText;
+        int oldWave = -1;
 
         public Renderer(GameModel model)
         {
@@ -41,6 +50,52 @@ namespace BlackMatter.Renderer
         {
             DrawingGroup dg = new DrawingGroup();
             dg.Children.Add(GetBackground());
+            dg.Children.Add(GetPlayer());
+            dg.Children.Add(GetEnemies());
+            dg.Children.Add(GetBullets());
+            //dg.Children.Add(GetWaves());
+
+            return dg;
+        }
+        /*
+        private Drawing GetWaves()
+        {
+            if (oldWave!=model.Wave)
+            {
+                formattedText = new FormattedText(model.Wave.ToString(), System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, font, 16, Brushes.Red, 1);
+                Text;
+            }
+            return formattedText;
+        }*/
+
+        private Drawing GetBullets()
+        {
+            if (Bullet == null)
+            {
+                Geometry g = new RectangleGeometry(new Rect(305,400, 5, 5));
+                Enemy = new GeometryDrawing(Brushes.Red, null, g);
+            }
+            return Enemy;
+        }
+
+        private Drawing GetEnemies()
+        {
+            if (Enemy == null)
+            {
+                Geometry g = new RectangleGeometry(new Rect(300,400, 25, 25));
+                Enemy= new GeometryDrawing(Brushes.Red, null, g);
+            }
+            return Enemy;
+        }
+
+        private Drawing GetPlayer()
+        {
+            if (Player == null)
+            {
+                Geometry g = new RectangleGeometry(new Rect(model.player.X, model.player.Y, 25,25 ));
+                Player = new GeometryDrawing(PlayerBrush, null, g);                
+            }
+            return Player;
         }
 
         private Drawing GetBackground()
